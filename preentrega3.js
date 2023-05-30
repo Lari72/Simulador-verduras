a3bafa0bc54a1e21142d9a8fa79d9e515af9eb66
simulador de compras online <laricu72@gmail.com>
2023-05-29 22:20:47 -0400

    fotos

--- preentrega3.js
+++ preentrega3.js
@@ -16,7 +16,7 @@ class Producto {
 };
 
 const productos = [
-    {id:1, nombre: "Cebolla", precio: 1500, imagen: "../Images/Cebolla.jpg", cantidad:2, descripcion: " Cebolla Verde, fresca de origen local"},
+    {id:1, nombre: "Cebolla", precio: 1500, imagen: "..//Images/Cebolla.jpg", cantidad:2, descripcion: " Cebolla Verde, fresca de origen local"},
     {id:2, nombre: "Lechuga", precio: 1000, imagen: "../Images/Lechuga.jpg", cantidad:2, descripcion: "Lechuga del dia, origen zona central"},
     {id:3, nombre: "Tomates", precio: 2500, imagen: "../Images/Tomates.jpg", cantidad:1, descripcion: "Tomates de alta calidad, origen norte"},
     {id:4, nombre: "Zanahoria", precio: 1300, imagen: "../Images/Zanahoria.jpg", cantidad:1, descripcion: "Zanahoria calidad premium, origen zona central"},
